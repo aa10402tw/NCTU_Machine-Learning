1b0242bb343bd1f2bab51d74c67fe314ddaa3d1a
unknown <aa10402tw@gmail.com>
2018-09-26 20:03:01 +0800

    09/26

--- HW 1/0756079_hw1.py
+++ HW 1/0756079_hw1.py
@@ -6,58 +6,64 @@ import matplotlib.pyplot as plt
 def rLES(xs, ys, n_basis, lambda_):
 	'''
 	formula:
-		equation : x = (ATA + lambda * I)^(-1) * ATb 
-		A_ = (ATA + lambda * I)
-		b_ = AT * b
+		equation : x = (A_T * A + lambda * I)^(-1) * A_T * b 
+		A_ = (A_T * A + lambda * I)
+		b_ = A_T * b
 		Solve A_*x = b_ with LU to get x 
 	'''
 	A = ChenKW_Matrix([[ x**i for i in range(n_basis) ] for x in xs])
-	b = ChenKW_Matrix([ys]).T()
+	b = ChenKW_Matrix([ys]).T
 	I = ChenKW_Matrix.identity(n_basis)
 	lambda_I = lambda_ * I 
 
-	A_ = (A.T() * A + lambda_I )
-	b_ = A.T() * b
+	A_ = (A.T * A + lambda_I )
+	b_ = A.T * b
 	x = ChenKW_Matrix.LU_solve(A_, b_)
 
 	formula = 'y = ' 
-	for i, w in enumerate(x.T()[0]):
+	for i, w in enumerate(x.T[0]):
 		if i > 0 :
 			formula += ' + '
 		formula += '(%.2f)(x^%i)'%(w, i)
 	print('The Formula is : [ %s ]' %(formula) )
-	error =  ((A*x - b).T() * (A*x - b))[0][0] + lambda_ * (x.T() * x)[0][0]
-	print('The Error is : [ %.2f ]'%error)
+	error = ((A*x-b).T * (A*x-b)) + lambda_*(x.T*x)
+	error = error.value
+	print('The Error is : [ %.2f ]'% (error))
 	return x
 
 
 def Newton(xs, ys, n_basis, x0=None, max_iter=100, step_size=1):
 	'''
 	formula:
-		x1 = x0 - (f'(x)/f''(x))
+		equation : x_new = x - step_size*(f'(x)/f''(x)) 
+			or x_new = x - step_size*inv(Hession)*gradient
+		let y = inv(Hession)*gradient, A = Hession, and b = gradient
+		we can get y by solving Ay = b using LU decompsition
 	'''
 	if x0 is None:
 		x0 = ChenKW_Matrix([[0] for i in range(n_basis)])
 	A = ChenKW_Matrix([[ x**i for i in range(n_basis) ] for x in xs])
-	b = ChenKW_Matrix([ys]).T()
+	b = ChenKW_Matrix([ys]).T
 	x = x0
 	for i in range(max_iter):
-		gradient =  (2 * A.T() * A * x) -  ( 2 * A.T() * b)
-		hession = (2 *A.T() * A) 
-		x_new = x - step_size * hession.inv() * gradient
+		A_ = hession = (2 *A.T * A) 
+		b_ = gradient =  (2 * A.T * A * x) -  ( 2 * A.T * b)
+		step_dir = y = ChenKW_Matrix.LU_solve(A_, b_)
+		x_new = x - (step_size * step_dir)
 		if(x_new == x):
-			print('Converge after %i iteration'%(i+1))
+			print('Converge after %i iteration'%(i))
 			x = x_new
 			break
 		x = x_new
 
 	formula = 'y = ' 
-	for i, w in enumerate(x.T()[0]):
+	for i, w in enumerate(x.T[0]):
 		if i > 0 :
 			formula += ' + '
 		formula += '(%.2f)(x^%i)'%(w, i)
 	print('The Formula is : [ %s ]' %(formula) )
-	error =  ((A*x - b).T() * (A*x - b))[0][0]
+	error =  ((A*x-b).T * (A*x-b))
+	error = error.value
 	print('The Error is : [ %.2f ]'%error)
 	return x
 
@@ -82,18 +88,18 @@ def read_data(file_name):
 # lambda_ = args.lambda_
 
 file_path = 'test.txt'
-n_basis = 4
-lambda_ = 0.1
+n_basis = 7
+lambda_ = 0
 xs, ys = read_data(file_path)
 
-print('-' * 100)
-print("Least Square error with regulation")
+print('#' + '-' * 100 )
+print("Least Square error with regularization")
 rLES(xs, ys, n_basis, lambda_)
+print( '-' * 100 + '#')
 
-print('-' * 100)
-
+print('\n#' + '-' * 100 )
 print("Newton' method for optimization")
 Newton(xs, ys, n_basis)
-print('-' * 100)
+print( '-' * 100 + '#')
 
 
--- HW 1/ChenKW_matrix.py
+++ HW 1/ChenKW_matrix.py
@@ -1,6 +1,4 @@
 
-import numpy as np
-
 ''' self-implemented matrix class (NCTU CS 0756079, Chen Kuan-Wen ) '''
 class ChenKW_Matrix: 
 
@@ -11,23 +9,43 @@ class ChenKW_Matrix:
 		self.precision = 2
 		self.shape = (self.n_row, self.n_col)
 
+	@property
+	def T(self):
+		''' see transpose(A) in detail'''
+		return ChenKW_Matrix.transpose(self)
+
+	@property
+	def I(self):
+		''' see inverse(A) in detail'''
+		return ChenKW_Matrix.inverse(self)
+
+	@property
+	def value(self):
+		if self.n_row == 1 and self.n_col == 1:
+			return self.mat[0][0]
+		else:
+			return self.mat
+
 	################################################
 	### Operator Overloading make it easy to use ###
 	################################################
+	''' Compare two matrics are equal or not '''
 	def __eq__(self, mat):
 		if isinstance(mat, ChenKW_Matrix):
 			if mat.shape == self.shape:
    				for row_1, row_2 in zip(mat, self):
    					for num_1, num_2 in zip(row_1, row_2):
-   						if  not (num_1 - num_2 < 0.000001):
+   						if  not (num_1 - num_2 < 0.0001):
    							return False
    				return True
 		else:
 			return False
 
+	''' define what will A[i] return '''
 	def __getitem__(self,key):
 		return self.mat[key]
 
+	''' define what will happen when A[i] = k is called '''
 	def __setitem__(self,key,value):
 		self.mat[key] = value 
 
@@ -36,17 +54,15 @@ class ChenKW_Matrix:
 		if isinstance(B, int) or isinstance(B, float):
 			return ChenKW_Matrix([[self.mat[i][j] * B for j in range(self.n_col)]  for i in range(self.n_row)])
 		if not self.n_col == B.n_row:
-			print('Wrong dimension while mat mul') 
-			return None
+			raise Exception('Wrong dimension while mat mul')
 		resultMat = [[sum( [self.mat[i][j] * B.mat[j][k] for j in range(self.n_col)] )  for k in range(B.n_col) ]  for i in range(self.n_row)]
 		return ChenKW_Matrix(resultMat)
 	__rmul__ = __mul__
 
-	''' IImplementation of matrix addition'''
+	''' Implementation of matrix addition'''
 	def __add__(self, B):
 		if not ( self.n_row == B.n_row and self.n_col == B.n_col):
-			print('Wrong dimension while mat add') 
-			return None
+			raise Exception('Wrong dimension while mat add')
 		resultMat = [[self.mat[i][j] + B.mat[i][j] for j in range(self.n_col)]  for i in range(self.n_row)]
 		return ChenKW_Matrix(resultMat)
 	__radd__ = __add__
@@ -54,16 +70,15 @@ class ChenKW_Matrix:
 	''' Implementation of matrix subtraction'''
 	def __sub__(self, B):
 		if not ( self.n_row == B.n_row and self.n_col == B.n_col):
-			print('Wrong dimension while mat add') 
-			return None
+			raise Exception('Wrong dimension while mat sub')
 		resultMat = [[self.mat[i][j] - B.mat[i][j] for j in range(self.n_col)]  for i in range(self.n_row)]
 		return ChenKW_Matrix(resultMat)
 
-
+	''' define what will return when call len(mat) '''
 	def __len__(self):
 		return len(self.mat)
 
-	''' Define what will be printed  when call print(mat) '''
+	''' Define what will be printed when call print(mat) '''
 	def __str__(self): 
 		max_len = 0
 		for i in range(self.n_row):
@@ -86,7 +101,7 @@ class ChenKW_Matrix:
 	### Useful Methods ###
 	######################
 	def row_op(self, type, i, j=0, k=0):
-		''' see copyMatrix(A) in detail '''
+		''' see rowOperation() in detail '''
 		return ChenKW_Matrix.rowOperation(self, type, i, j, k)
 
 	def copy(self):
@@ -95,10 +110,9 @@ class ChenKW_Matrix:
 
 	def inv(self):
 		''' see inverse(A) in detail '''
-		return ChenKW_Matrix(np.linalg.inv(self.mat))
 		return ChenKW_Matrix.inverse(self)
 
-	def T(self):
+	def trans(self):
 		''' see transpose(A) in detail'''
 		return ChenKW_Matrix.transpose(self)
 
@@ -124,13 +138,13 @@ class ChenKW_Matrix:
 	''' Implementation of matrix row operation'''
 	@staticmethod
 	def rowOperation(mat, type, i, j=0, k=0):
-		if type == 1: # mult row i for k times
+		if type == 1:  # mult row_i for k times
 			mat[i] = [ mat[i][col] * k for col in range(len(mat[i]))]
 			return ChenKW_Matrix(mat)
-		elif type == 2: # row exchange
+		elif type == 2: # row_i exchange with row_j
 			mat[i], mat[j] = mat[j], mat[i]
 			return ChenKW_Matrix(mat)
-		elif type == 3:
+		elif type == 3: # Add row_i * k to row_j
 			mat[j] = [ mat[j][col] + k * mat[i][col] for col in range(len(mat[i]))]
 			return ChenKW_Matrix(mat)
 
@@ -138,13 +152,13 @@ class ChenKW_Matrix:
 	@staticmethod
 	def concatenate(A1, A2, axis=0):
 		A = []
-		if(axis == 0):
+		if(axis == 0): # Vertically concatenate
 			for A1_row in A1:
 				A.append(A1_row)
 			for A2_row in A2:
 				A.append(A2_row)
 			
-		if(axis == 1):
+		elif(axis == 1): # Horizationally concatenate
 			for A1_row, A2_row in zip(A1, A2):
 				A.append(A1_row + A2_row) # List append
 		return ChenKW_Matrix(A)
@@ -152,7 +166,6 @@ class ChenKW_Matrix:
 	''' 
 	Implementation of Matrix Transpose
 		formula: B = A^T, where b_ij = a_ji
-
 	'''
 	@staticmethod 
 	def transpose(A):
@@ -179,7 +192,7 @@ class ChenKW_Matrix:
 			for row in range(d+1, L_b.n_row):
 				m = -1 * (L_b[row][d] / L_b[d][d])
 				L_b = L_b.row_op(type=3, i=d, j=row, k=m)
-		y = ChenKW_Matrix([[row[-1] for row in L_b]]).T()
+		y = ChenKW_Matrix([[row[-1] for row in L_b]]).T
 
 		# solve Ux = y
 		U_y = ChenKW_Matrix.concatenate(U, y, axis=1)
@@ -188,7 +201,7 @@ class ChenKW_Matrix:
 			for row in range(d-1, -1, -1):
 				m = -1 * (U_y[row][d] / U_y[d][d])
 				U_y = U_y.row_op(type=3, i=d, j=row, k=m)
-		x = ChenKW_Matrix([[row[-1] for row in U_y]]).T() 
+		x = ChenKW_Matrix([[row[-1] for row in U_y]]).T 
 		return x
 
 	''' 
@@ -210,21 +223,50 @@ class ChenKW_Matrix:
 				L[i][j] = (A[i][j] - sum([U[k][j]*L[i][k] for k in range(j)]) ) / U[j][j]
 		return ChenKW_Matrix(L), ChenKW_Matrix(U)
 
+	
+	''' 
+	Implementation of Matrix Inverse using Gaussian-Jordan elimination
+		formula:
+			[ A | I ] --> [ I | inv(A) ]  where '-->' represent row operations 
+ 	'''
+	@staticmethod
+	def inverse(A):
+		A = ChenKW_Matrix.copyMatrix(A)
+		if not A.n_row == A.n_col:
+			raise Exception('Not Square matrix has no inverse.')
+		n = A.n_row
+		I = ChenKW_Matrix.identity(n)
+		A_I = ChenKW_Matrix.concatenate(A, I, axis=1)
+		for d in range(n):
+			# Make Sure diagonal is non-zero
+			for row in range(d, n):
+				if not A_I[row][d] == 0:
+					A_I = A_I.row_op(type=2, i=d, j=row) # exchange non-zero row to diagonal
+					break
+			if A_I[d][d] == 0:
+				raise Exception('Singular matrix has no inverse.')
+
+			# Do row operations to make the elements of column is 1 if diagonal, 0 otherwise 
+			A_I = A_I.row_op(type=1, i=d, k=(1/A_I[d][d])) 
+			for row in range(n):
+				if d==row: continue
+				m = -1*(A_I[row][d]/A_I[d][d])
+				A_I = A_I.row_op(type=3, i=d, j=row, k=m)
+		A_inv = ChenKW_Matrix([row[n:] for row in A_I])
+		return A_inv
+
+
 		
 
 if __name__ == '__main__':
 	print_precision = 2
 	A = ChenKW_Matrix( [[7, 3, -1, 2], [3, 8, 1, -4], [-1, 1, 4, -1], [2, -4, -1, 6]] )
-	# print(ChenKW_Matrix.concatenate(A,B, axis=1))
 	print(A)
-	print(A.inv() )
-	print(A.inv().inv())
+	print(A.inv())
 	print(A.inv() * A)
-	# print(ChenKW_Matrix.inverse(A_inv))
-	# print(L)
-	# print(U)
-# L, U = LU(A)
-# print_matrix(L)
-# print_matrix(U)
-# print(matmul(A,B))
+	print(A.inv().inv())
+	b = ChenKW_Matrix( [[7, 3, -1, 2]] )
+	print(b)
+	print(b.T)
+
 
